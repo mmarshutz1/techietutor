9a34be6a90b5727fc6055aef588d66913f0c9d88
mmarshutz1 <mmarshutz@gmail.com>
2024-08-23 01:15:48 -0700

    WEBSITE

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const consultationHeader = document.querySelector('.consultation-header');
+const consultationForm = document.querySelector('#consultation-form');
+
+document.querySelector('.free-consultation-btn').addEventListener('click', function() {
+    consultationHeader.classList.add('slide-in');
+    document.querySelector('.consultation-form').style.display = 'block';
+});
+
+consultationForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Send email using EmailJS
+    emailjs.sendForm('service_qusbchp', 'template_cdfwsep', consultationForm)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            consultationHeader.classList.add('slide-out');
+            setTimeout(function() {
+                consultationHeader.classList.remove('slide-in', 'slide-out');
+                document.querySelector('.consultation-form').style.display = 'none';
+            }, 500);
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+});
+// "Animated" Header Background
+document.querySelector('.header').addEventListener('mousemove', function(e) {
+    const { offsetX, offsetY, target } = e;
+    const { offsetWidth: width, offsetHeight: height } = target;
+    const moveX = (offsetX / width) * 5; // Smaller multiplier for slower movement
+    const moveY = (offsetY / height) * 5;
+
+    target.style.backgroundPosition = `${50 + moveX}% ${50 + moveY}%`;
+});
+
