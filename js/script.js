76026e2de9e39b58bb0a55073800a61f1baad194
mmarshutz1 <mmarshutz@gmail.com>
2024-08-29 18:32:08 -0700

    Update 2.2.3
    Test

--- js/script.js
+++ js/script.js
@@ -40,10 +40,10 @@ document.getElementById('options').addEventListener('change', function() {
     var submenu1 = document.getElementById('submenu1');
     var submenu2Container = document.getElementById('submenu2-container');
 
-    if (selectedValue === 'Repair') {
+    if (selectedValue === 'repair') {
         submenu1.style.display = 'block';
         submenu2Container.style.display = 'none';
-    } else if (selectedValue === 'Tutoring') {
+    } else if (selectedValue === 'tutoring') {
         submenu1.style.display = 'none';
         submenu2Container.style.display = 'flex';
     } else {
